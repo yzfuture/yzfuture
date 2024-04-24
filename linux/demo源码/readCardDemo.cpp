bea0d5dc8e6722ea390378df369b25787b305485
yzfuture <admin@yzfuture.cn>
2024-04-24 13:54:37 +0800

    添加了linux下webapi和websocket的接口，并且对部分接口进行了调整

--- linux/demo源码/readCardDemo.cpp
+++ linux/demo源码/readCardDemo.cpp
@@ -25,170 +25,184 @@ int readCardInfo(int nindex)
 	std::string	szerror = "";
 	int			nerr;
 
-	YZWLHandle hlHandle = cardOpenDeviceEx(
-		(char*)szServerIP.c_str(),
-		nServerPort,
-		2,
-		nerr, 
-		nindex);
-	if (hlHandle >= 0)
+	if (nindex == 0)
 	{
-		cardBeep(hlHandle);
-		
-		char	arrcardNo[32] = { 0 };
-		int		ncardNo = sizeof(arrcardNo);
-		if (getACardNo(hlHandle, arrcardNo, ncardNo))
+		setDeviceType(0);
+	}
+	else
+	{
+		if (nindex < 1001)
 		{
-			printf("getACardNo[%s]\r\n", arrcardNo);
+			nindex = 1001;
 		}
-		else
-		{
-			printf("The card is no a type\r\n");
-		}
-		
-		if (setCardType(hlHandle))
-		{						
-			bool		bmove(true);
-			bool		bfind(cardFindCard(hlHandle, bmove));
-			if (bfind)
-			{
-				char		szSN[100] = { 0 };
-				int			nSNlen = 99;
-				if (cardGetCardSN(hlHandle, szSN, nSNlen))
-				{
-					printf("cardGetCardSN[%s]\r\n", szSN);
+		setDeviceType(1);
+	}
 
-					CardInfoStruct cardinfo;
-					bool		bret = cardReadTwoCardEx(hlHandle, &onCardReadProgress, NULL, cardinfo);
-					int		nSerialNumber(0);
-					char	szSerialNumber[1024] = { 0 };
-					cardGetSerialNumber(hlHandle, szSerialNumber, nSerialNumber);
-					printf("Serial Number [%s]\r\n", szSerialNumber);
-					if (!bret)
+	if (loginCardServerEx((char*)szServerIP.c_str(), nServerPort, nerr))
+	{
+		YZWLHandle hlHandle = cardOpenDevice(2, nerr, nindex);
+		if (hlHandle >= 0)
+		{
+			cardBeep(hlHandle);
+
+			char	arrcardNo[32] = { 0 };
+			int		ncardNo = sizeof(arrcardNo);
+			if (getACardNo(hlHandle, arrcardNo, ncardNo))
+			{
+				printf("getACardNo[%s]\r\n", arrcardNo);
+			}
+			else
+			{
+				printf("The card is no a type\r\n");
+			}
+
+			if (setCardType(hlHandle))
+			{
+				bool		bmove(true);
+				bool		bfind(cardFindCard(hlHandle, bmove));
+				if (bfind)
+				{
+					char		szSN[100] = { 0 };
+					int			nSNlen = 99;
+					if (cardGetCardSN(hlHandle, szSN, nSNlen))
 					{
-						int		nlen(0);
-						char	szError[1024] = { 0 };
-						cardGetErrorInfo(hlHandle, szError, nlen, cardGetLastErrorCode(hlHandle));
-					}
-					else
-					{
-						bsuccess = true;
-						printf("\033[34mDecode success, converting Unicode to ANSI to get Chinese  +++++++++++++\r\n");
-						
-						unsigned char*	lpphoto = nullptr;
-						if (cardinfo.etype == (char)eOldForeignerType)
+						printf("cardGetCardSN[%s]\r\n", szSN);
+
+						CardInfoStruct cardinfo;
+						bool		bret = cardReadTwoCardEx(hlHandle, &onCardReadProgress, NULL, cardinfo);
+						int		nSerialNumber(0);
+						char	szSerialNumber[1024] = { 0 };
+						cardGetSerialNumber(hlHandle, szSerialNumber, nSerialNumber);
+						printf("Serial Number [%s]\r\n", szSerialNumber);
+						if (!bret)
 						{
-							printf("\r\n========Foreigner's Residence Permit(old)==========\r\n");
-							ForeignerInfoOld	oldInfo;
-							memset(&oldInfo, 0, sizeof(ForeignerInfoOld));
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrEnName, sizeof(cardinfo.info.foreigner.arrEnName), oldInfo.arrEnName);
-							printf("EnName:%s\r\n", oldInfo.arrEnName);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrName, sizeof(cardinfo.info.foreigner.arrName), oldInfo.arrName);
-							printf("Name:%s\r\n", oldInfo.arrName);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrSex, sizeof(cardinfo.info.foreigner.arrSex), oldInfo.arrSex);
-							printf("Sex:%s\r\n", oldInfo.arrSex);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrNo, sizeof(cardinfo.info.foreigner.arrNo), oldInfo.arrNo);
-							printf("NO:%s\r\n", oldInfo.arrNo);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrCountry, sizeof(cardinfo.info.foreigner.arrCountry), oldInfo.arrCountry);
-							printf("Country:%s\r\n", oldInfo.arrCountry);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrBirthday, sizeof(cardinfo.info.foreigner.arrBirthday), oldInfo.arrBirthday);
-							printf("Birthday:%s\r\n", oldInfo.arrBirthday);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrValidityPeriodBegin, sizeof(cardinfo.info.foreigner.arrValidityPeriodBegin), oldInfo.arrValidityPeriodBegin);
-							printf("BeginData:%s\r\n", oldInfo.arrValidityPeriodBegin);
-							convertUncodeToUtf8(cardinfo.info.foreigner.arrValidityPeriodEnd, sizeof(cardinfo.info.foreigner.arrValidityPeriodEnd), oldInfo.arrValidityPeriodEnd);
-							printf("EndDate:%s\r\n", oldInfo.arrValidityPeriodEnd);
-							lpphoto = cardinfo.info.foreigner.arrPhoto;
-						}
-						else if (cardinfo.etype == (char)eNewForeignerType)
-						{
-							printf("\r\n========Foreigner's Residence Permit(new)==========\r\n");
-							ForeignerInfoNew newForeigner;
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrEnName, sizeof(cardinfo.info.newForeigner.arrEnName), newForeigner.arrEnName);
-							printf("EnName:%s\r\n", newForeigner.arrEnName);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrName, sizeof(cardinfo.info.newForeigner.arrName), newForeigner.arrName);
-							printf("Name:%s\r\n", newForeigner.arrName);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrNo, sizeof(cardinfo.info.newForeigner.arrNo), newForeigner.arrNo);
-							printf("NO:%s\r\n", newForeigner.arrNo);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrCountry, sizeof(cardinfo.info.newForeigner.arrCountry), newForeigner.arrCountry);
-							printf("Country:%s\r\n", newForeigner.arrCountry);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrBirthday, sizeof(cardinfo.info.newForeigner.arrBirthday), newForeigner.arrBirthday);
-							printf("Birthday:%s\r\n", newForeigner.arrBirthday);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrValidityPeriodBegin, sizeof(cardinfo.info.newForeigner.arrValidityPeriodBegin), newForeigner.arrValidityPeriodBegin);
-							printf("BeginData:%s\r\n", newForeigner.arrValidityPeriodBegin);
-							convertUncodeToUtf8(cardinfo.info.newForeigner.arrValidityPeriodEnd, sizeof(cardinfo.info.newForeigner.arrValidityPeriodEnd), newForeigner.arrValidityPeriodEnd);
-							printf("EndDate:%s\r\n", newForeigner.arrValidityPeriodEnd);
-							lpphoto = cardinfo.info.newForeigner.arrPhoto;
+							int		nlen(0);
+							char	szError[1024] = { 0 };
+							cardGetErrorInfo(hlHandle, szError, nlen, cardGetLastErrorCode(hlHandle));
 						}
 						else
 						{
-							if (cardinfo.etype == (char)eTwoGATType) printf("\r\n========Hong Kong, Macao, and Taiwan Residence Permit==========\r\n");
-							else printf("\r\n========TwoID==========\r\n");
-							TwoIdInfoStruct twoId;
-							convertUncodeToUtf8(cardinfo.info.twoId.arrNo, sizeof(cardinfo.info.twoId.arrNo), twoId.arrNo);
-							printf("NO:%s\r\n", twoId.arrNo);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrName, sizeof(cardinfo.info.twoId.arrName), twoId.arrName);
-							printf("Name:%s\r\n", twoId.arrName);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrAddress, sizeof(cardinfo.info.twoId.arrAddress), twoId.arrAddress);
-							printf("Address:%s\r\n", twoId.arrAddress);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrBirthday, sizeof(cardinfo.info.twoId.arrBirthday), twoId.arrBirthday);
-							printf("Birthday:%s\r\n", twoId.arrBirthday);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrValidityPeriodBegin, sizeof(cardinfo.info.twoId.arrValidityPeriodBegin), twoId.arrValidityPeriodBegin);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrValidityPeriodEnd, sizeof(cardinfo.info.twoId.arrValidityPeriodEnd), twoId.arrValidityPeriodEnd);
-							printf("Date:%s~%s\r\n", twoId.arrValidityPeriodBegin, twoId.arrValidityPeriodEnd);
-							convertUncodeToUtf8(cardinfo.info.twoId.arrSignedDepartment, sizeof(cardinfo.info.twoId.arrSignedDepartment), twoId.arrSignedDepartment);
-							printf("SignedDepartment:%s\r\n", twoId.arrSignedDepartment);
-							lpphoto = cardinfo.info.twoId.arrPhoto;
-						}
-						if (lpphoto)
-						{
-							char szBmp[1024 * 40] = { 0 };
-							int outlen = 1024 * 40;
-							if (decodeCardImage(lpphoto, szBmp, &outlen))
+							bsuccess = true;
+							printf("\033[34mDecode success, converting Unicode to ANSI to get Chinese  +++++++++++++\r\n");
+							convertCardInfoUncodeToUtf8Ex(cardinfo);
+
+							unsigned char*	lpphoto = nullptr;
+							if (cardinfo.etype == (char)eOldForeignerType)
 							{
-								FILE* fpwlt(fopen("a.wlt", "wb"));
-								if (fpwlt)
+								printf("\r\n========Foreigner's Residence Permit(old)==========\r\n");
+								ForeignerInfoOld	oldInfo = cardinfo.info.foreigner;
+								memset(&oldInfo, 0, sizeof(ForeignerInfoOld));
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrEnName, sizeof(cardinfo.info.foreigner.arrEnName), oldInfo.arrEnName);
+								printf("EnName:%s\r\n", oldInfo.arrEnName);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrName, sizeof(cardinfo.info.foreigner.arrName), oldInfo.arrName);
+								printf("Name:%s\r\n", oldInfo.arrName);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrSex, sizeof(cardinfo.info.foreigner.arrSex), oldInfo.arrSex);
+								printf("Sex:%s\r\n", oldInfo.arrSex);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrNo, sizeof(cardinfo.info.foreigner.arrNo), oldInfo.arrNo);
+								printf("NO:%s\r\n", oldInfo.arrNo);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrCountry, sizeof(cardinfo.info.foreigner.arrCountry), oldInfo.arrCountry);
+								printf("Country:%s\r\n", oldInfo.arrCountry);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrBirthday, sizeof(cardinfo.info.foreigner.arrBirthday), oldInfo.arrBirthday);
+								printf("Birthday:%s\r\n", oldInfo.arrBirthday);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrValidityPeriodBegin, sizeof(cardinfo.info.foreigner.arrValidityPeriodBegin), oldInfo.arrValidityPeriodBegin);
+								printf("BeginData:%s\r\n", oldInfo.arrValidityPeriodBegin);
+								// convertUncodeToUtf8(cardinfo.info.foreigner.arrValidityPeriodEnd, sizeof(cardinfo.info.foreigner.arrValidityPeriodEnd), oldInfo.arrValidityPeriodEnd);
+								printf("EndDate:%s\r\n", oldInfo.arrValidityPeriodEnd);
+								lpphoto = cardinfo.info.foreigner.arrPhoto;
+							}
+							else if (cardinfo.etype == (char)eNewForeignerType)
+							{
+								printf("\r\n========Foreigner's Residence Permit(new)==========\r\n");
+								ForeignerInfoNew newForeigner = cardinfo.info.newForeigner;
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrEnName, sizeof(cardinfo.info.newForeigner.arrEnName), newForeigner.arrEnName);
+								printf("EnName:%s\r\n", newForeigner.arrEnName);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrName, sizeof(cardinfo.info.newForeigner.arrName), newForeigner.arrName);
+								printf("Name:%s\r\n", newForeigner.arrName);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrNo, sizeof(cardinfo.info.newForeigner.arrNo), newForeigner.arrNo);
+								printf("NO:%s\r\n", newForeigner.arrNo);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrCountry, sizeof(cardinfo.info.newForeigner.arrCountry), newForeigner.arrCountry);
+								printf("Country:%s\r\n", newForeigner.arrCountry);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrBirthday, sizeof(cardinfo.info.newForeigner.arrBirthday), newForeigner.arrBirthday);
+								printf("Birthday:%s\r\n", newForeigner.arrBirthday);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrValidityPeriodBegin, sizeof(cardinfo.info.newForeigner.arrValidityPeriodBegin), newForeigner.arrValidityPeriodBegin);
+								printf("BeginData:%s\r\n", newForeigner.arrValidityPeriodBegin);
+								// convertUncodeToUtf8(cardinfo.info.newForeigner.arrValidityPeriodEnd, sizeof(cardinfo.info.newForeigner.arrValidityPeriodEnd), newForeigner.arrValidityPeriodEnd);
+								printf("EndDate:%s\r\n", newForeigner.arrValidityPeriodEnd);
+								lpphoto = cardinfo.info.newForeigner.arrPhoto;
+							}
+							else
+							{
+								if (cardinfo.etype == (char)eTwoGATType) printf("\r\n========Hong Kong, Macao, and Taiwan Residence Permit==========\r\n");
+								else printf("\r\n========TwoID==========\r\n");
+								TwoIdInfoStruct twoId = cardinfo.info.twoId;
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrNo, sizeof(cardinfo.info.twoId.arrNo), twoId.arrNo);
+								printf("NO:%s\r\n", twoId.arrNo);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrName, sizeof(cardinfo.info.twoId.arrName), twoId.arrName);
+								printf("Name:%s\r\n", twoId.arrName);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrAddress, sizeof(cardinfo.info.twoId.arrAddress), twoId.arrAddress);
+								printf("Address:%s\r\n", twoId.arrAddress);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrBirthday, sizeof(cardinfo.info.twoId.arrBirthday), twoId.arrBirthday);
+								printf("Birthday:%s\r\n", twoId.arrBirthday);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrValidityPeriodBegin, sizeof(cardinfo.info.twoId.arrValidityPeriodBegin), twoId.arrValidityPeriodBegin);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrValidityPeriodEnd, sizeof(cardinfo.info.twoId.arrValidityPeriodEnd), twoId.arrValidityPeriodEnd);
+								printf("Date:%s~%s\r\n", twoId.arrValidityPeriodBegin, twoId.arrValidityPeriodEnd);
+								// convertUncodeToUtf8(cardinfo.info.twoId.arrSignedDepartment, sizeof(cardinfo.info.twoId.arrSignedDepartment), twoId.arrSignedDepartment);
+								printf("SignedDepartment1:%s\r\n", twoId.arrSignedDepartment);
+								lpphoto = cardinfo.info.twoId.arrPhoto;
+							}
+							if (lpphoto)
+							{
+								char szBmp[1024 * 40] = { 0 };
+								int outlen = 1024 * 40;
+								if (decodeCardImage(lpphoto, szBmp, &outlen))
 								{
-									fwrite(lpphoto, 1024, 1, fpwlt);
-									fclose(fpwlt);
-								}
-								FILE* fp(fopen("a.bmp", "wb"));
-								if (fp)
-								{
-									fwrite(szBmp, outlen, 1, fp);
-									fclose(fp);
+									FILE* fpwlt(fopen("a.wlt", "wb"));
+									if (fpwlt)
+									{
+										fwrite(lpphoto, 1024, 1, fpwlt);
+										fclose(fpwlt);
+									}
+									FILE* fp(fopen("a.bmp", "wb"));
+									if (fp)
+									{
+										fwrite(szBmp, outlen, 1, fp);
+										fclose(fp);
+									}
 								}
 							}
+							printf("\033[0m");
 						}
-						printf("\033[0m");
-					}		
+					}
 				}
+			}
+			else
+			{
+				int		nlen(0);
+				char	szError[1024] = { 0 };
+				cardGetErrorInfo(hlHandle, szError, nlen, cardGetLastErrorCode(hlHandle));
+				printf("set card type failed\r\n");
 			}
 		}
 		else
 		{
 			int		nlen(0);
 			char	szError[1024] = { 0 };
-			cardGetErrorInfo(hlHandle, szError, nlen, cardGetLastErrorCode(hlHandle));
-			printf("set card type failed\r\n");
+			cardGetErrorInfo(hlHandle, szError, nlen, -19997);
+			printf("open device failed\r\n");
+		}
+		if (hlHandle >= 0)
+		{
+			cardCloseDevice(hlHandle);
+			hlHandle = -1;
 		}
 	}
-	else
-	{
-		int		nlen(0);
-		char	szError[1024] = { 0 };
-		cardGetErrorInfo(hlHandle, szError, nlen, -19997);
-		printf("open device failed\r\n");
-	}
-	if (hlHandle >= 0)
-	{
-		cardCloseDevice(hlHandle);
-		hlHandle = -1;
-	}
+
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
+	cardReadInit();
 	char	cexit = '0';
 	do
 	{
@@ -206,5 +220,6 @@ int main(int argc, char *argv[])
 		}
 	} while (true);
 	getchar();
+	cardReadUninit();
 	return 0;
 }
--- linux/sdk/include/readCardInfoInterface.h
+++ linux/sdk/include/readCardInfoInterface.h
@@ -10,13 +10,23 @@ extern "C" {
 
 	typedef void(FBC_API_PUBLIC *cardReadProgress)(void* userData, unsigned int nProgress);
 
-	YZWLHandle FBC_API_PUBLIC cardOpenDeviceEx(char* szip, int nport, int nouttime, int &nerr, int nDeviceNo = 0);
-	bool FBC_API_PUBLIC setCardType(YZWLHandle nDeviceHandle);
+	void FBC_API_PUBLIC cardReadInit(); // 初始化操作，最开始程序构造的时候调用
+	void FBC_API_PUBLIC setDeviceType(int nDeviceType);
+
+	bool FBC_API_PUBLIC loginCardServerEx(char* szip, int nport, int &nerr);
+	YZWLHandle FBC_API_PUBLIC cardOpenDevice(int nouttime, int &nerr, int nDeviceNo = 0);
+
+	bool FBC_API_PUBLIC setCardType(YZWLHandle nDeviceHandle, cardType ctype=BCardType);
 	bool FBC_API_PUBLIC cardFindCard(YZWLHandle nDeviceHandle, bool &bmove);
 
 	bool FBC_API_PUBLIC cardGetCardDN(YZWLHandle nDeviceHandle, char* szsn, int &nlen);
 	bool FBC_API_PUBLIC cardGetCardSN(YZWLHandle nDeviceHandle, char* szsn, int &nlen);
+
 	bool FBC_API_PUBLIC getACardNo(YZWLHandle nDeviceHandle, char* arrcardNo, int &nlen);
+	bool FBC_API_PUBLIC authACardAPwd(YZWLHandle ldevHandle, int nsector, char* szpwd);
+	bool FBC_API_PUBLIC authACardBPwd(YZWLHandle ldevHandle, int nsector, char* szpwd);
+	bool FBC_API_PUBLIC readACardData(YZWLHandle ldevHandle, int nblock/*nblock = nsector*4+index */, char* szread);
+	bool FBC_API_PUBLIC writeACardData(YZWLHandle ldevHandle, int nblock/*nblock = nsector*4+index */, char* szwrite);
 
 	bool FBC_API_PUBLIC cardGetDeviceNO(YZWLHandle nDeviceHandle, char* szno, int &nlen);
 	bool FBC_API_PUBLIC cardGetDeviceSN(YZWLHandle nDeviceHandle, char* szsn, int &nlen);
@@ -37,11 +47,17 @@ extern "C" {
 	void FBC_API_PUBLIC cardCloseDevice(YZWLHandle nDeviceHandle);
 
 	int	FBC_API_PUBLIC	convertUncodeToUtf8(char* Src, int nsrc, char* strOut);
+	void 	FBC_API_PUBLIC convertCardInfoUncodeToUtf8(TwoIdInfoStructEx &cardinfo);
+	void 	FBC_API_PUBLIC convertCardInfoUtf8ToUncode(TwoIdInfoStructEx &cardinfo);
+	void 	FBC_API_PUBLIC convertCardInfoUncodeToUtf8Ex(CardInfoStruct &cardinfo);
 
 	// srcimage长度固定为1024 
 	// outimage大小由外面自己申请，大小不小于40KB
 	// outlen传入时为outimage实际大小，传出时为实际图片大小
 	bool FBC_API_PUBLIC decodeCardImage(unsigned char* srcimage, char* outimage, int* outlen);
+
+	void 	FBC_API_PUBLIC logoutCardServer();
+	void 	FBC_API_PUBLIC cardReadUninit(); // 反初始化操作，最后程序析构的时候调用
 
 
 #ifdef __cplusplus
